4caf3a2d1e54beb2a3c165b97f657bcbfc8719be
Aakash Shah <69837586+thecodewithaakash@users.noreply.github.com>
2022-08-20 13:04:23 +0530

    Updated Script.js
    Updated Script.js

--- script.js
+++ script.js
@@ -38,14 +38,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Freelancer","Blogger","YouTuber","Web Developer"],
+        strings: [ "Freelancer","Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Freelancer","Blogger","YouTuber","Web Developer"],
+        strings: [ "Freelancer","Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
